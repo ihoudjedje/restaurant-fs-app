969dde65eedae4cb8e1bb5e8cb83a4c73b1a62a9
ilyeSudo <houdjedje.ilyes@gmail.com>
2020-11-02 20:17:48 +0100

    feat: start implementing items service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ItemsController } from './items/items.controller';
 import { AppService } from './app.service';
+import { ItemsService } from './items/items.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ItemsController],
-  providers: [AppService],
+  providers: [AppService, ItemsService],
 })
 export class AppModule {}
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,14 +1,17 @@
-import { Get, Post, Controller } from '@nestjs/common';
+import { Get, Post, Body, Controller } from '@nestjs/common';
+import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
   async findAll(): Promise<string[]> {
-    return ['Pizza', 'Coke'];
+    return this.itemsService.findAll();
   }
 
   @Post()
-  async create() {
-    return 'Not yet implemented';
+  async create(@Body() item: string) {
+    this.itemsService.create(item);
   }
 }
