55e628191d9a7aa1fb1c4e17e4f32287578f7b1e
ilyeSudo <houdjedje.ilyes@gmail.com>
2020-11-04 11:00:00 +0100

    feat: add auth service to ng app

--- ng-frontend/src/app/app.module.ts
+++ ng-frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './auth/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemsComponent],
+  declarations: [AppComponent, ItemsComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
